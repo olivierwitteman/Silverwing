40a7a019570f393b4fc39e2722b3c412af1b7cf6
Olivier Witteman <olivier@2001.net>
2019-02-20 18:48:07 +0100

    Matrix

--- BTS/data/final_data/Analyze.py
+++ BTS/data/final_data/Analyze.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from scipy.signal import lfilter
 import numpy as np
+import scipy.signal as scs
 
 '''OCV'''
 
@@ -9,11 +10,27 @@ mode = 0
 
 # R_battery = 0.0
 R_battery = 6.*0.0128/4.
+textsize = 17.5
+width = 7.5
 
 path = './'
 # path = '/Users/olivierwitteman/Downloads/'
 # name = 'BT-B-1_1'
-name = 'BT-E-1800_1200-80_40-6'
+# name = 'BT-E-1800_1200-80_40-6_PCC'
+name = 'US18650_VTC6_fp_feb'
+
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = scs.butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = scs.filtfilt(b, a, data)
+    return y
 
 
 def remove_last(Us, ts, Is, Tsp, Tsa, As):
@@ -41,43 +58,47 @@ with open('{!s}{!s}.log'.format(path, name), 'r') as data:
     samples = data.readlines()
     Us, Is, ts, As, Tsp, Tsa, rmrk = [], [], [], [0.], [], [], []
 
-for i in np.arange(0, len(samples), 10):
+for i in np.arange(0, len(samples), 1):
     try:
+
         Us.append(float(samples[i].split()[1][1:].strip()))
         ts.append(float(samples[i].split()[0][1:].strip()))
         Is.append(float(samples[i].split()[2][1:].strip()))
         # print(float(samples[i].split()[3][3:-2].strip()))
-        Tsa.append(float(samples[i].split()[3][3:].strip()))
+        # Tsa.append(float(samples[i].split()[3][3:].strip()))
         # print(float(samples[i].split()[4][3:-2].strip()))
-        Tsp.append(float(samples[i].split()[4][3:].strip()))
+        Tsp.append(float(samples[i].split()[3][1:-2].strip()))
 
-        if i > 1:
+        # if Tsp[-1] < -100.:
+        #     raise KeyboardInterrupt
+
+        if i > 0:
             dt = ts[-1] - ts[-2]
             As.append(float(As[-1] + Is[-1] * dt / 3600.))
 
-        if Is[-1] > 0 or Tsp[-1] < -100. or Tsa[-1] < -100.:
-            # print(len(Us))
-            raise KeyboardInterrupt
+            if Tsp[-1] < 10:
+                Tsa = [0, 0]
+                raise KeyboardInterrupt
 
     except:
         # print('ass\n\n\n')
-        # print('Us: ', len(Us))
+        Tsa = [0, 0]
         Us, ts, Is, Tsp, Tsa, As = remove_last(Us, ts, Is, Tsp, Tsa, As)
-        # print('Us: ', len(Us))
         pass
 
 
-maxlength = min(len(Us), len(ts), len(Is), len(Tsp), len(Tsa), len(As))
+maxlength = min(len(Us), len(ts), len(Is), len(Tsp), len(As))
 Us = Us[:maxlength]
 ts = ts[:maxlength]
 Is = Is[:maxlength]
 Tsp = Tsp[:maxlength]
-Tsa = Tsa[:maxlength]
+# Tsa = Tsa[:maxlength]
 As = As[:maxlength]
 
 As = [-x + max(As) for x in As]
 
-fig, ax1 = plt.subplots(figsize=(10, 6.25))
+
+fig, ax1 = plt.subplots(figsize=(width, width/1.6))
 colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'r', 'g', 'c']
 
 # start, stop = ts.index(d_s_mark[0]) + 1, ts.index(d_e_mark[0]) - 1
@@ -90,9 +111,8 @@ R_total = R_sys + R_battery
 ax2 = ax1.twinx()
 ax2.grid()
 
-endurance = round((ts[-1]-ts[0])/60., 1)
-n = 3  # the larger n is, the smoother curve will be
-
+endurance = round((ts[-1]-ts[0])/60., 0)
+n = 15  # the larger n is, the smoother curve will be
 
 dOCV = np.array([R_total * -x for x in Is[:]])
 OCV = np.array(Us[:]) + dOCV
@@ -100,11 +120,18 @@ OCV = np.array(Us[:]) + dOCV
 av_current = round(-np.average(Is), 1)
 av_voltage = np.average(OCV)
 
+# Filter requirements.
+order = 1
+fs = 10.  # sample rate, Hz
+cutoff = .1  # desired cutoff frequency of the filter, Hz
+
+Tsp_butter = butter_lowpass_filter(Tsp, cutoff, fs, order=order)
+
 # smooth_Us = lfilter([1.0 / n] * n, 1, Us[start:stop])[1:]
 smooth_Us = lfilter([1.0 / n] * n, 1, OCV)
 smooth_Is = -lfilter([1.0 / n] * n, 1, Is[:])
 smooth_Tsp = lfilter([1.0 / n] * n, 1, Tsp[:])
-smooth_Tsa = lfilter([1.0 / n] * n, 1, Tsa[:])
+# smooth_Tsa = lfilter([1.0 / n] * n, 1, Tsa[:])
 
 A_zero = min(As[:])
 As_i = [x - A_zero for x in As[:]]
@@ -113,29 +140,49 @@ caps.append(capacity)
 actual_energy = round(capacity*av_voltage, 1)
 energy = round(capacity * sum(smooth_Us)/len(smooth_Us), 1)
 
-
-ax1.plot(As_i, smooth_Us, c=colors[0], ls='-', label='I_avg = {!s}A, E_extracted = {!s}Wh, endurance = {!s}min'.
-         format(av_current, actual_energy, endurance))
+c = np.array(smooth_Is)/capacity
 
 
-ax2.plot(As_i, smooth_Tsp, c=colors[0], ls='-.', label='{!s}A [T]'.format(av_current))
+ax1.scatter(np.nan, np.nan, label='$E$ = {!s} Wh'.format(actual_energy))
+ax1.scatter(np.nan, np.nan, label='$t$ = {!s} mins'.format(endurance))
+ax1.plot(As_i, smooth_Us, c=colors[0], ls='-', label='Voltage')
+ax2.plot(As_i, Tsp_butter, c='r', ls='-.', label='Temperature'.format(av_current))
+ax1.plot(As_i, c, c='g', ls=':', label='C-rate'.format(av_current), lw=2.)
+
+
+ax1.set_xlabel('Capacity [Ah]', fontsize=textsize)
+ax1.set_ylabel('Voltage [V], Disch. Rate', fontsize=textsize)
+ax2.set_ylabel('Temperature [deg C]', fontsize=textsize)
+
+label1 = 'I_avg = {!s}A, E_extracted = {!s}Wh, endurance = {!s}min'\
+    .format(av_current, actual_energy, endurance)
+
+
+# ax2.plot(As_i, Tsp_butter, c=colors[0], ls='-.', label='{!s}A [T]'.format(av_current))
+
 # ax2.plot(As_i, smooth_Tsa, c=colors[0], ls='-.', label='{!s}A [T]'.format(av_current))
 
-ax2.plot(As_i, smooth_Is, c=colors[0], ls=':', label='{!s}A [A]'.format(av_current), lw=2.)
-ax1.set_xlabel('Capacity [Ah]')
-ax1.set_ylabel('Open Circuit Voltage [V]')
-ax2.set_ylabel('Temperature [deg C], Current [A]')
-ax1.set_xlim(0, 1.1*max(caps))
-ax1.set_ylim(10, 30)
-ax2.set_ylim(0, 80)
+# ax2.plot(As_i, smooth_Is, c=colors[0], ls=':', label='{!s}A [A]'.format(av_current), lw=2.)
 
-ax1.plot(np.nan, np.nan, ls=':', label='Current', c='k', lw=2.)
-ax1.plot(np.nan, np.nan, ls='-.', label='Temperature', c='k', lw=1.5)
+ax1.set_xlim(0, 1.02*max(caps))
+ax1.set_ylim(0, 7)
+ax2.set_ylim(20, 45)
 
-plt.title('ID: {!s}, charged CC-CV at 0.7C with cutoff at 0.05C'.format(name))
+# ax1.plot(np.nan, np.nan, ls=':', label='Current', c='k', lw=2.)
+ax1.plot(np.nan, np.nan, ls='-.', label='Temperature', c='r')
 
-ax1.legend(loc=0)
+# plt.title('ID: {!s}, charged CC-CV at 0.7C with cutoff at 0.05C'.format(name))
+# plt.title('Sony VTC6 at flight profile', fontsize=textsize)
+
+ax1.legend(loc='center', fontsize=textsize, ncol=3, bbox_to_anchor=(0.5, 1.18))
+
+# ax1.set_yticks([20, 30, 40, 50, 60, 70])
+# ax2.set_yticks([0, 2.5, 5.])
+ax1.tick_params(labelsize=14)
+ax2.tick_params(labelsize=14)
+
+plt.tight_layout()
 
 ax1.grid(True)
-plt.savefig('./{!s}.png'.format(name), dpi=250, format='png')  # Use eps for LaTeX, other options: png, pdf, ps, eps
+plt.savefig('./{!s}.eps'.format(name), dpi=255, format='eps')  # Use eps for LaTeX, other options: png, pdf, ps, eps
 plt.show()
--- BTS/data/final_data/Crate_Temp.py
+++ BTS/data/final_data/Crate_Temp.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import lfilter
+import scipy.signal as scs
 
 R_battery = 6.*0.0128/4.
+textsize = 15
+interval = 1
 
 name = 'BT-E-1800_1200-80_40-6'
 namePCC = 'BT-E-1800_1200-80_40-6_PCC'
@@ -12,10 +15,11 @@ namecell = 'US18650_VTC6_fp_feb'
 # axarr[1] = plt.subplot(212)
 
 # Two subplots, the axes array is 1-d
-fig, axarr = plt.subplots(2, sharex=True, gridspec_kw={'height_ratios': [2, 1]})
+width = 7.5
+fig, axarr = plt.subplots(2, sharex=True, gridspec_kw={'height_ratios': [2, 1]}, figsize=(width, width/1.5))
 
 
-def plot(x, y, label, axis='l', color='b'):
+def plot(x, y, label, axis='l', color='b', linetype='-'):
     # You can change plot to scatter or whatever if you'd like
     # colors: b, g, r, c, m, y, k, w or HEX values as '#eeefff'
 
@@ -23,11 +27,11 @@ def plot(x, y, label, axis='l', color='b'):
         # axarr[0].plot(x, y)
         # axarr[1].plot(np.nan, np.nan, label=label)
 
-        axarr[0].plot(x, y, label=label, c=color)
+        axarr[0].plot(x, y, label=label, c=color, ls=linetype)
 
     elif axis == 'l':
         # axarr[1].plot(x, y, label=label)
-        axarr[1].plot(x, y, label=label, c=color)
+        axarr[1].plot(x, y, label=label, c=color, ls=linetype)
 
     else:
         print('Please choose axis="l" or axis="r"')
@@ -43,33 +47,44 @@ def remove_last(Us, ts, Is, Tsp, Tsa, As, Cs):
     As = As[:-2]
     Cs = Cs[:-2]
 
-    # new_array = []
-    # print array
-    #
-    # for j in range(len(array)):
-    #     new_array.append([array[j][:-2]])
-    #
-    # print new_array
-    # return new_array
-
     return Us, ts, Is, Tsp, Tsa, As, Cs
 
 
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = scs.butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = scs.filtfilt(b, a, data)
+    return y
+
+
 def pack_data(name):
+    # Filter requirements.
+    order = 5
+    fs = 10.  # sample rate, Hz
+    cutoff = 0.01  # desired cutoff frequency of the filter, Hz
+
     path = './'
     with open('{!s}{!s}.log'.format(path, name), 'r') as data:
         samples = data.readlines()
         Us, Is, ts, As, Tsp, Tsa, rmrk, Cs = [], [], [], [0.], [], [], [], []
 
-    for i in np.arange(0, len(samples), 10):
+    for i in np.arange(0, len(samples), interval):
         try:
             Us.append(float(samples[i].split()[1][1:].strip())/6.)
             ts.append(float(samples[i].split()[0][1:].strip()))
             Is.append(float(samples[i].split()[2][1:].strip())/4.)
             Cs.append(-Is[-1]/3.)
             Tsa.append(float(samples[i].split()[3][3:].strip()))
-            Tsp.append(float(samples[i].split()[4][3:].strip()))
-
+            if name == 'BT-E-1800_1200-80_40-6':
+                Tsp.append(float(samples[i].split()[4][3:].strip()) + 8.)
+            else:
+                Tsp.append(float(samples[i].split()[4][3:].strip()))
             if i > 1:
                 dt = ts[-1] - ts[-2]
                 As.append(float(As[-1] + Is[-1] * dt / 3600.))
@@ -80,7 +95,7 @@ def pack_data(name):
 
         except:
             Us, ts, Is, Tsp, Tsa, As, Cs = remove_last(Us, ts, Is, Tsp, Tsa, As, Cs)
-            # pass
+            pass
 
     maxlength = min(len(Us), len(ts), len(Is), len(Tsp), len(Tsa), len(As))
     Us = Us[:maxlength]
@@ -89,6 +104,7 @@ def pack_data(name):
     Tsp = Tsp[:maxlength]
     Tsa = Tsa[:maxlength]
     As = As[:maxlength]
+    Cs = Cs[:maxlength]
 
     As = [-x + max(As) for x in As]
 
@@ -96,7 +112,6 @@ def pack_data(name):
 
     R_sys = 0.0
     R_total = R_sys + R_battery
-
 
     endurance = round((ts[-1] - ts[0]) / 60., 1)
     n = 10  # the larger n is, the smoother curve will be
@@ -107,7 +122,6 @@ def pack_data(name):
     av_current = round(-np.average(Is), 1)
     av_voltage = np.average(OCV)
 
-    # smooth_Us = lfilter([1.0 / n] * n, 1, Us[start:stop])[1:]
     smooth_Us = lfilter([1.0 / n] * n, 1, OCV)
     smooth_Is = -lfilter([1.0 / n] * n, 1, Is[:])
     smooth_Tsp = lfilter([1.0 / n] * n, 1, Tsp[:])
@@ -115,21 +129,30 @@ def pack_data(name):
 
     A_zero = min(As[:])
     As_i = [x - A_zero for x in As[:]]
+
+    Tsp_butter = butter_lowpass_filter(Tsp, cutoff, fs, order=order)
+
     capacity = max(As_i)
+    As_i = [x*100./capacity for x in As_i]
+
     caps.append(capacity)
     actual_energy = round(capacity * av_voltage, 1)
     energy = round(capacity * sum(smooth_Us) / len(smooth_Us), 1)
 
-    return As_i, smooth_Us, smooth_Is, smooth_Tsp, Cs
+    return As_i, smooth_Us, smooth_Is, Tsp_butter, Cs, ts
 
 
 def cell_data(name):
+    # Filter requirements.
+    order = 1
+    fs = 10.  # sample rate, Hz
+    cutoff = 0.1  # desired cutoff frequency of the filter, Hz
     with open('./{!s}.log'.format(name), 'r') as data:
         samples = data.readlines()
 
         Us, Is, ts, As, c_s_mark, d_s_mark, c_e_mark, d_e_mark, Ts, rmrk, Cs = [], [], [], [0.], [], [], [],\
                                                                                [], [], [], []
-        for i in range(len(samples)):
+        for i in range(0, len(samples), interval):
             try:
                 Us.append(float(samples[i].split()[1][1:].strip()))
                 ts.append(float(samples[i].split()[0][1:].strip()))
@@ -140,44 +163,50 @@ def cell_data(name):
                     rmrk.append(str(samples[i].split()[6][:].strip()))
                 except:
                     pass
-
-                if float(Ts[-1]) == -101.:
-                    d_s_mark.append(ts[-1])
-
-                elif float(Ts[-1]) == -102.:
-                    d_e_mark.append(ts[-1])
-
-                elif float(Ts[-1]) == -103.:
-                    c_s_mark.append(ts[-1])
-
-                elif float(Ts[-1]) == -104.:
-                    c_e_mark.append(ts[-1])
+                #
+                # if float(Ts[-1]) == -101.:
+                #     d_s_mark.append(ts[-1])
+                #
+                # elif float(Ts[-1]) == -102.:
+                #     d_e_mark.append(ts[-1])
+                #
+                # elif float(Ts[-1]) == -103.:
+                #     c_s_mark.append(ts[-1])
+                #
+                # elif float(Ts[-1]) == -104.:
+                #     c_e_mark.append(ts[-1])
 
                 if i > 0:
                     dt = ts[-1] - ts[-2]
                     As.append(float(As[-1] + Is[-1] * dt / 3600.))
+
+                    if Ts[-1] < 10 or Is[-1] > -1.:
+                        Tsa = [0, 0]
+                        Us, ts, Is, Ts, Tsa, As, Cs = remove_last(Us, ts, Is, Ts, Tsa, As, Cs)
 
             except:
                 pass
 
     As = [-x + max(As) for x in As]
 
-    start, stop = ts.index(d_s_mark[0]) + 1, ts.index(d_e_mark[0]) - 1
-    av_current = abs(round(sum(Is[start:stop]) / (stop - start), 1))
-    av_voltage = round(sum(Us[start:stop]) / (stop - start), 1)
+    maxlength = min(len(Us), len(ts), len(Is), len(Ts), len(As))
+    Us = Us[:maxlength]
+    ts = ts[:maxlength]
+    Is = Is[:maxlength]
+    Ts = Ts[:maxlength]
+    As = As[:maxlength]
+    Cs = Cs[:maxlength]
+
+
+    # av_current = abs(round(sum(Is[start:stop]) / (stop - start), 1))
+    # av_voltage = round(sum(Us[start:stop]) / (stop - start), 1)
     caps = []
 
     R_sys = 0.008
     R_total = R_sys + R_battery
 
-    # print R_sys
-    # lst = [0, -1]
-
-    j = 0
-    # for j in range(len(d_s_mark)):  # '-1!!!!!!!!!!'
-
-    start, stop = ts.index(d_s_mark[j]) + 1, ts.index(d_e_mark[j]) - 1
-    endurance = round((d_e_mark[j] - d_s_mark[j]) / 60., 1)
+    start, stop = 0, -1
+    endurance = round((ts[stop] - ts[start])/60.)
     n = 10  # the larger n is, the smoother curve will be
 
     av_voltage = round(sum(Us[start:stop]) / (stop - start), 1)
@@ -193,40 +222,63 @@ def cell_data(name):
 
     A_zero = min(As[start:stop])
     As_i = [x - A_zero for x in As[start:stop]]
+
     capacity = max(As_i)
+
+    As_i = [x*100./capacity for x in As_i]
+
     caps.append(capacity)
+
+    Ts_butter = butter_lowpass_filter(Ts[start:stop], cutoff, fs, order=order)
+
     actual_energy = round(capacity * av_voltage, 1)
     energy = round(capacity * sum(smooth_Us) / len(smooth_Us), 1)
 
-    return As_i, smooth_Us, smooth_Is, smooth_Ts, Cs
+    # return As_i, smooth_Us, smooth_Is, Ts[start:stop], Cs
+    return As_i, smooth_Us, smooth_Is, Ts_butter, Cs[start:stop]
 
 
-As0, Us0, Is0, Tsp0, Cs0 = pack_data(name)
-# As1, Us1, Is1, Tsp1, Cs1 = pack_data(namePCC)
+As0, Us0, Is0, Tsp0, Cs0, ts0 = pack_data(name)
+As1, Us1, Is1, Tsp1, Cs1, ts1 = pack_data(namePCC)
 As2, Us2, Is2, Tsp2, Cs2 = cell_data(namecell)
 
-start = 10
-plot(As0[start:], Tsp0[start:], 'Pack w/o PCC', 'u', color='b')
-plot(As2[start:], Tsp2[start:], 'Individual cell', 'u', color='g')
-plot(As0[:], Cs0[:], 'C-rate', 'l', color='k')
+start = 0
+# plot(As0[start:], Tsp0[start:], 'Pack w/o PCC', 'u', color='b', linetype='-.')
+# plot(As1[start:], Tsp1[start:], 'Pack with PCC', 'u', color='r', linetype='-')
+# plot(As2[start:], Tsp2[start:], 'Cell', 'u', color='g', linetype='--')
 
-title = 'Normalized BTS thermal data'
 
-axarr[0].set_title(title)
+plot(As0[start:], Tsp0[start:], 'Pack w/o PCC', 'u', color='b', linetype='-.')
+plot(As1[start:], Tsp1[start:], 'Pack with PCC', 'u', color='r', linetype='-')
+# plot(ts[start:], Tsp2[start:], 'Cell', 'u', color='g', linetype='--')
 
-axarr[1].set_xlabel('Capacity [Ah]')
+plot(As2[:], Cs2[:], 'C-rate', 'l', color='k')
+# plot(ts1[:], Cs1[:], 'C-rate', 'l', color='k')
 
-axarr[0].set_ylabel('Temperature [deg C]')
-axarr[0].set_ylim(15, 70)
+title = 'BTS thermal data'
 
-axarr[1].set_ylabel('Discharge rate [c]')
+# axarr[0].set_title(title, fontsize=textsize)
+
+axarr[1].set_xlabel('Depth of Discharge [%]', fontsize=textsize)
+
+axarr[0].set_ylabel('Temp. [deg C]', fontsize=textsize)
+axarr[0].set_ylim(10, 70)
+
+axarr[1].set_ylabel('Disch. rate [c]', fontsize=textsize)
 axarr[1].set_ylim(0, 7)
 
-axarr[0].legend(loc=0)
-axarr[1].legend(loc=0)
+axarr[0].legend(loc='upper center', fontsize=textsize, ncol=3, bbox_to_anchor=(0.5, 1.3))
+# axarr[1].legend(loc='center', fontsize=textsize, ncol=3, bbox_to_anchor=(0.5, 1.15))
 axarr[0].grid(True)
 axarr[1].grid(True)
 
 
-# plt.savefig('./{!s}'.format(title), dpi=255, format='eps')  # Use eps for LaTeX, other options: png, pdf, ps, eps
+axarr[0].set_yticks([20, 30, 40, 50, 60, 70])
+axarr[1].set_yticks([0, 2.5, 5.])
+axarr[0].tick_params(labelsize=14)
+axarr[1].tick_params(labelsize=14)
+
+plt.tight_layout()
+
+plt.savefig('./{!s}'.format('BTS_Thermal.eps'), dpi=255, format='eps')  # Use eps for LaTeX, other options: png, pdf, ps, eps
 plt.show()

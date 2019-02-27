5a81e71ca85f02cd3fd71c7497d29c87ab37aae2
Olivier Witteman <olivier@2001.net>
2019-02-27 09:03:02 +0100

    i2c

--- PTS/Pitot.py
+++ PTS/Pitot.py
@@ -1,5 +1,6 @@
 import smbus
 import time
+import math
 
 bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 
@@ -7,6 +8,12 @@ bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 DEVICE_ADDRESS = 0x28      #7 bit address (will be left shifted to add the read write bit)
 DEVICE_REG_MODE1 = 0
 DEVICE_REG_MODE2 = 1
+
+
+def cas(qc, p0):
+    v = 661.4788 * math.sqrt(5.*((qc/p0 + 1.)**(2./7.) - 1))
+    return v
+
 
 try:
     while True:
@@ -17,6 +24,8 @@ try:
         lst.sort()
         print min(lst), max(lst)
         print sum(lst)/len(lst), lst[int(len(lst)/2)]
+
+        print(cas(max(lst)/1000., lst[int(len(lst)/2)]/1000.))
         # print(bus.read_word_data(DEVICE_ADDRESS, DEVICE_REG_MODE2))
         print('\n\n')
         time.sleep(1)

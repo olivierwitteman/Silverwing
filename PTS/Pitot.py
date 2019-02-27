378e5835d95a598a61efdc8aab318d2449e2f986
Olivier Witteman <olivier@2001.net>
2019-02-27 08:00:40 +0100

    i2c

--- PTS/Pitot.py
+++ PTS/Pitot.py
@@ -4,15 +4,15 @@ import time
 bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 
 
-# DEVICE_ADDRESS = 0x28      #7 bit address (will be left shifted to add the read write bit)
-DEVICE_ADDRESS = 28
+DEVICE_ADDRESS = 0x28      #7 bit address (will be left shifted to add the read write bit)
 DEVICE_REG_MODE1 = 0
 DEVICE_REG_MODE2 = 1
 
 try:
     while True:
-        print(bus.read_word_data(DEVICE_ADDRESS, DEVICE_REG_MODE1))
-        print(bus.read_word_data(DEVICE_ADDRESS, DEVICE_REG_MODE2))
+        for i in range(8):
+            print(bus.read_word_data(DEVICE_ADDRESS, i))
+        # print(bus.read_word_data(DEVICE_ADDRESS, DEVICE_REG_MODE2))
         print('\n\n')
         time.sleep(1)
 

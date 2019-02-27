6795e015f4a1c42ec53ed4edcc04459091072d0f
Olivier Witteman <olivier@2001.net>
2019-02-27 07:59:05 +0100

    i2c

--- PTS/Pitot.py
+++ PTS/Pitot.py
@@ -3,10 +3,11 @@ import time
 
 bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 
+
 DEVICE_ADDRESS = 0x28      #7 bit address (will be left shifted to add the read write bit)
-DEVICE_REG_MODE1 = 0x00
-DEVICE_REG_MODE2 = 0x02
-DEVICE_REG_LEDOUT0 = 0x1d
+# DEVICE_ADDRESS = 28
+DEVICE_REG_MODE1 = 0
+DEVICE_REG_MODE2 = 1
 
 try:
     while True:

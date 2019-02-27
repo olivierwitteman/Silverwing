30bece935fa69ded69e6e179245b54212cf3d5f1
Olivier Witteman <olivier@2001.net>
2019-02-27 10:36:56 +0100

    i2c

--- PTS/Pitot.py
+++ PTS/Pitot.py
@@ -29,10 +29,8 @@ def poll_q(delta=0.):
     print min(lst), max(lst)
     print sum(lst) / len(lst), lst[int(len(lst) / 2.)]
 
-    p0 = lst[int(len(lst) / 2)]  # inch mercury
-    q = ((abs(max(lst) - p0))/1000. - delta) * 3386.389
-
-    p0 = p0 * 3386.389/1000.
+    p0 = lst[int(len(lst) / 2)] * 3386.389/1000.  # inch mercury
+    q = ((abs(max(lst) - p0)) - delta) * 3386.389/1000.
 
     return q, p0
 

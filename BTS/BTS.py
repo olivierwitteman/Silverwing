71daf72ba9087ce2da0fc933b77c3addf4521a67
Olivier Witteman <olivier@2001.net>
2019-04-03 12:02:27 +0200

    test matrix BTS (PCC)

--- BTS/BTS.py
+++ BTS/BTS.py
@@ -226,9 +226,10 @@ def delta_discharge(name, minvolt, maxvolt, current, R, duration, status='empty'
             power = bat_voltage * a_current
             dp = power - req_power
 
-            current -= dp/bat_voltage
+            current += dp/bat_voltage
+            setcurrent = max(0., min(-current, maxcurrent))
             print('dp: {!s} W, current: {!s} A'.format(round(dp, 1), round(current, 2)))
-            delta.set_current(min(-current, maxcurrent))
+            delta.set_current(setcurrent)
 
             a_temp = temp_ambient()
             c_temp = temp_pack()

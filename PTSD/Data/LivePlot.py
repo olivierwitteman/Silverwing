a9fb36554b08b96db95847ec02c71f950347cb01
Olivier Witteman <olivier@2001.net>
2019-07-07 17:31:58 +0200

    Safety

--- PTSD/Data/LivePlot.py
+++ PTSD/Data/LivePlot.py
@@ -13,6 +13,7 @@ path = './'
 
 list_of_files = glob.glob('{!s}*.csv'.format(path)) # * means all if need specific format then *.csv
 latest_file = max(list_of_files, key=os.path.getctime)
+print('\nlatest file: {!s}\n'.format(latest_file))
 
 steps = 10
 

8667d5efa1c87211579befbda9580996d6231449
Olivier Witteman <olivier@2001.net>
2019-07-09 15:24:23 +0200

    Changed analog inputs inverter

--- PTSD/Data/LivePlot.py
+++ PTSD/Data/LivePlot.py
@@ -59,7 +59,7 @@ while True:
         samples = data.readlines()[1:]
 
     # lastline += len(samples)
-    for i in np.arange(max([-6000, it*-100*steps]), -1, steps):
+    for i in np.arange(max([-18000, it*-25*steps]), -1, steps):
         # timestamp
         timestamp.append(float(samples[i].split(',')[0][:].strip()))
         # relative time

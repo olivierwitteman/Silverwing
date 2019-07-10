0e305cff376a8771f47b1d95293cbaad9da5f7f5
Olivier Witteman <olivier@2001.net>
2019-07-10 15:19:57 +0200

    Watchdog test

--- Delta/watchdog.py
+++ Delta/watchdog.py
@@ -1,7 +1,12 @@
 import delta_sm3300 as d
 delta = d.DeltaComm()
+import time
 
+delta.set_voltage(0.1)
+delta.set_current(0.1)
+delta.set_state(1)
 
 delta.enable_watchdog()
+time.sleep(6)
 
 delta.close_connection()

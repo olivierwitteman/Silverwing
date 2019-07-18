47b1beff8ff445e1d8b26f494d9d1fab483cbe21
Olivier Witteman <olivier@2001.net>
2019-07-18 08:26:31 -0700

    Figuring out the problem

--- PTSD/Charge_Battery.py
+++ PTSD/Charge_Battery.py
@@ -72,6 +72,11 @@ try:
             delta.set_voltage(v_set)
             delta.set_current(I_set)
             delta.set_state(1)
+            time.sleep(1.)
+            print('{!s} > {!s}'.format(abs(v_set - delta.ask_voltage()), 0.1))
+            print('{!s} > {!s}'.format(delta.ask_current(), 0.1 * BPE['parallel'] *VTC6['capacity']))
+            print('{!s} < {!s}'.format(0.8 * BPE['series'] * VTC6['min_cell_volt'], delta.ask_voltage() < 1.001 * BPE['series'] * VTC6['max_cell_volt']))
+
             while abs(v_set - delta.ask_voltage()) > 0.1 and delta.ask_current() > 0.1 * BPE['parallel'] *\
                     VTC6['capacity'] and 0.8 * BPE['series'] * VTC6['min_cell_volt'] < delta.ask_voltage() < \
                     1.001 * BPE['series'] * VTC6['max_cell_volt']:

c3796cc1c4561ce2dadd0154667920acc7f9500d
olivierwitteman <olivier@2001.net>
2019-09-06 10:49:19 +0200

    dataaa

--- PTSD/Power_Management.py
+++ PTSD/Power_Management.py
@@ -31,7 +31,7 @@ def precharge():
 
     t0_pre, t = time.time(), time.time()
 
-    while t - t0_pre < 5.:
+    while t - t0_pre < 10.:
         print('\rPrecharge Voltage: {!s}, precharge current: {!s}, precharge power: {!s}'.format(round(delta.ask_voltage(), 2),
                                                                           round(delta.ask_current(), 2),
                                                                           round(delta.ask_power(), 2)), end='')

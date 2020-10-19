d4debd2fb81959a962d8d61d936a0ca9f839a140
olivierwitteman <olivierwitteman@users.noreply.github.com>
2020-10-19 16:53:17 +0200

    Update slackbot.py

--- Slack/slackbot.py
+++ Slack/slackbot.py
@@ -1,6 +1,6 @@
 from slackclient import SlackClient
 
-slack_token = 'xoxp-393387664416-394034945139-520496789715-1ed9777320aa3a0d3b56cb67a1e7edf5'
+slack_token = '/////'
 sc = SlackClient(slack_token)
 
 for _ in range(1):
